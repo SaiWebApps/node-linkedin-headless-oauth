29e0d5f46d19321cd4543bec1ff39741fd7aed3a
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-08-29 10:06:26 -0700

    Renamed linkedin-auth-utils to index.
    Main directory only contains the npm library/module.
    example directory contains an ExpressJS server that utilizes the npm
    library (parent directory).

---
+++ example/get-basic-profile-details.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+
+var linkedinAuthUtils = require('node-linkedin-headless-oauth');
+linkedinAuthUtils.run('config.json', app, function(req, res) {
+	res.redirect('/');
+});
+app.get('/', function(req, res) {
+	linkedinAuthUtils.linkedin.people.me(function(err, $in) {
+		console.log($in);
+	});
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log('Node app is running on port', port);
+});
--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-var app = require('express')();
+function authorize(linkedinAuthMgr, email, password, scope)
+{
+	var authUrl = linkedinAuthMgr.auth.authorize(scope);
+	var browser = new (require('node-horseman'))();
 
-var linkedinAuthUtils = require('./linkedin-auth-utils');
-linkedinAuthUtils.run('config.json', app, function(req, res) {
-	res.redirect('/')
-});
+	browser
+		.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+		.open(authUrl)
+		.type('input[name="session_key"]', email)
+		.type('input[name="session_password"]', password)
+		.click('input[name="authorize"]')
+		.wait(15000)
+		.close();
+}
 
-app.get('/', function(req, res) {
-	linkedinAuthUtils.linkedin.people.me(function(err, $in) {
-		console.log($in);
+function acquireAccessToken(linkedinAuthMgr, app, route, handleResponse) 
+{
+	app.get(route, function(req, res) {
+		linkedinAuthMgr.auth.getAccessToken(
+			res, req.query.code, req.query.state,
+			function(err, results) {
+				if (err) { 
+					throw err; 
+				}
+				exports.linkedin = linkedinAuthMgr.init(results.access_token);
+				return handleResponse(req, res);
+			}
+		);
 	});
-});
+}
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log('Node app is running on port', port);
-});
+exports.linkedin = null;
+
+exports.run = function(apiConfigFile, app, handleResponse) {
+	var apiConfig = require('jsonfile').readFileSync(apiConfigFile);
+
+	var linkedinAuthMgr = require('node-linkedin')(apiConfig.apiKey, 
+		apiConfig.secret, apiConfig.callbackUrl);
+	var route = require('url').parse(apiConfig.callbackUrl).path;
+
+	authorize(linkedinAuthMgr, apiConfig.email, apiConfig.password, 
+		apiConfig.scope);
+	acquireAccessToken(linkedinAuthMgr, app, route, handleResponse);
+};
